9fc560000bc75dc949f4bfa0b823d2ace4fdb2ae
bmugenya26@gmail.com <bmugenya26@gmail.com>
2018-11-16 17:14:09 +0300

    Created a function to check prime number

--- primeNumbers.py
+++ primeNumbers.py
@@ -1,1 +1,9 @@
-
+def prime_number(num):
+    if type(num) == int and num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                return ("%d is not a prime number") % (num)
+            else:
+                return ("%d is a prime number") % (num)
+    else:
+        return "Unexpected Input"
